f5c331ba15de14c418cc7d7c0ddbcb020c4036f2
Matias Sciutto <sciuttomatias@gmail.com>
2021-06-09 19:28:36 -0300

    Ultimos cambios realizados por Matias

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
+import React from 'react'
+import { Route } from "react-router-dom";
+import Home from './components/content/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Barber Shop</h1> 
+      <Route exact path="/" component={Home}/>
     </div>   
   );
 }
 
 export default App;
+
+
+// import React from 'react'
+// import { Route } from "react-router-dom";
+// import Landing from './components/Landing/Landing';
+// import Home from './components/Home/Home';
+// import CountryDetail from './components/CountryDetail/CountryDetail';
+// import AddActivity from './components/AddActivity/AddActivity';
+// import {useSelector} from 'react-redux';
+
+
+// function App() {
+//   const countries = useSelector(store => store.countries);
+//   return (
+//     <div className="App">
+//       <Route exact path="/" component={Landing}/>
+//       <Route exact path="/countries" component={Home}/>
+//       <Route exact path="/countries/searched" component={Home}/>
+//       <Route exact path="/countryDetail/:id" component={CountryDetail}/>
+//       <Route exact path="/addActivity" component={AddActivity} countries={countries}/>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ client/src/components/content/Home/Home.jsx
@@ -0,0 +1,20 @@
+import styles from './Home.module.css';
+// importar Footer
+// importar Navbar
+
+function Home() {
+    
+    return (
+        <div className={styles.home}>
+            <h1>"Welcome to the Barber community"</h1>
+          
+            <div className={styles.hero}>
+                
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default Home;
