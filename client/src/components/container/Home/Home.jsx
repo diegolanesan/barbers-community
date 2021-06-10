1b57e827e8787308001d714b0129e150174140cd
Matias Sciutto <sciuttomatias@gmail.com>
2021-06-10 14:42:58 -0300

    Testing changes

--- client/src/components/container/Home/Home.jsx
+++ client/src/components/container/Home/Home.jsx
@@ -1,11 +1,12 @@
 import styles from './Home.module.css';
-import Image from 'react-bootstrap/Image'
 // importar Footer
 
 function Home() {
     
     return (
-        <Image src="https://buenosairesbarbershop.com/assets/img/bg/barberia_byn.jpg" fluid />
+        <div className={styles.divhome}>
+            
+        </div>
     )
 }
 
