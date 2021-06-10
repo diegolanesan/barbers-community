b78249f4854bcf9b8fbe2f88e1f50a2c4cf41580
Francisco Granja <franbbr110@gmail.com>
2021-06-10 17:46:26 -0300

    Tailwind

--- client/src/components/container/Home/Home.jsx
+++ client/src/components/container/Home/Home.jsx
@@ -2,10 +2,10 @@ import styles from './Home.module.css';
 // importar Footer
 
 function Home() {
-    
+
     return (
         <div className={styles.divhome}>
-            
+
         </div>
     )
 }
