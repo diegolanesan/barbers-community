f3b023e7c590ea49ee62b356a5ec54f588144c27
Matias Sciutto <sciuttomatias@gmail.com>
2021-06-11 18:48:59 -0300

    SQL sample file updated

--- api/sql_samples.sql
+++ api/sql_samples.sql
@@ -11,7 +11,6 @@ INSERT INTO "hairTypes" (id, description) VALUES (2, 'Crespo');
 INSERT INTO "hairTypes" (id, description) VALUES (3, 'Liso');
 INSERT INTO "hairTypes" (id, description) VALUES (4, 'Ondulado');
 ​
-​
 INSERT INTO "styles" (id, description) VALUES (1, 'American');
 INSERT INTO "styles" (id, description) VALUES (2, 'Clasic');
 INSERT INTO "styles" (id, description) VALUES (3, 'Fresco');
@@ -19,10 +18,17 @@ INSERT INTO "styles" (id, description) VALUES (4, 'Juvenil');
 INSERT INTO "styles" (id, description) VALUES (5, 'Modern');
 INSERT INTO "styles" (id, description) VALUES (6, 'Versátil');
 ​
-​
 select * from "hairTypes";
 select * from "faceTypes";
 select * from "style";
+
+INSERT INTO "categories" (id , name , description , image , "createdAt" ,"updatedAt" ) VALUES (1, 'HAIRCUT','CORTE DE CABELLO', '{IMAGE1,image2}', '2021-06-10', '2021-06-10');
+INSERT INTO "categories" (id , name , description , image , "createdAt" ,"updatedAt" ) VALUES (2, 'BEARDCUT','ARREGLO DE BARBA', '{IMAGE1,image2}', '2021-06-10  00:00:00-05', '2021-06-10 00:00:00-05');
+INSERT INTO "categories" (id , name , description , image , "createdAt" ,"updatedAt" ) VALUES (3, 'KIDHAIRCUT','ARREGLO DE BARBA', '{IMAGE1,image2}', '2021-06-10  00:00:00-05', '2021-06-10 00:00:00-05');
+INSERT INTO "categories" (id , name , description , image , "createdAt" ,"updatedAt" ) VALUES (1, 'DESIGN','ARREGLO DE BARBA', '{IMAGE1,image2}', '2021-06-10  00:00:00-05', '2021-06-10 00:00:00-05');
+INSERT INTO "categories" (id , name , description , image , "createdAt" ,"updatedAt" ) VALUES (1, 'OZON','ARREGLO DE BARBA', '{IMAGE1,image2}', '2021-06-10  00:00:00-05', '2021-06-10 00:00:00-05');
+INSERT INTO "categories" (id , name , description , image , "createdAt" ,"updatedAt" ) VALUES (1, 'MASK','ARREGLO DE BARBA', '{IMAGE1,image2}', '2021-06-10  00:00:00-05', '2021-06-10  00:00:00-05');
+SELECT * FROM CATEGORIES;
 
 /*​
 
---
+++ api/src/controllers/appointments.js
@@ -0,0 +1,125 @@
+const { Client, FaceType, HairType, Style } = require('../db');
+require('dotenv').config();
+const { Op } = require('sequelize');
+
+
+const getAppointments = (req, res, next) => {
+
+/* ----------------------- El usuario (administrador) busca todos los clientes ---------------------------------*/
+    if(!req.query.name){
+        try {
+            Appointment.findAll({
+                include: [ { model: FaceType }, {model: HairType}, { model: Style } ]  // CAMBIAR DATOS
+            })
+            .then((result) => {
+                res.status(200).send(result);
+            })
+        } catch(e){
+            console.log("No se pudo realizar la petición HTTP correctamente " + e);
+        }
+/*---------------------- El usuario (administrador) busca por nombre /client?name=pablo ---------------------*/
+    } else {
+        let queryName = req.query.name;         //REVISAR ESTA LÓGICA
+        try {
+            Client.findAll({ 
+                where: {name: {[Op.iLike]: `%${queryName}%`}},
+                include: [ { model: FaceType }, {model: HairType}, { model: Style } ]  // REVISAR SINTAXIS
+            })
+            .then((result) => {
+                res.status(200).send(result);
+            })
+        } catch {
+            res.status(404).send("Client not found!");
+        }
+    }
+}
+
+
+const getClientById = (req, res, next) => {
+    try {
+        let queryId = req.params.id.toUpperCase();
+        Client.findOne({
+            where: {id: queryId},
+            //include: {model: Activity}
+        })
+        .then((result) => {
+            res.status(200).send(result);
+        })
+        } catch(e){
+        console.log("No se pudo realizar la petición HTTP correctamente " + e);
+    }
+}
+
+
+const addClient = (req, res, next) => {
+    
+    const { 
+        name, 
+        lastname,
+        email,
+        image,
+        mobile, 
+        location, 
+        password, 
+        status, 
+        styleId, 
+        faceTypeId, 
+        hairTypeId } = req.body;
+    
+    try {
+        const createdClient = Client.create({ 
+            name, 
+            lastname, 
+            email, 
+            image, 
+            mobile, 
+            location, 
+            password, 
+            status, 
+            styleId, 
+            faceTypeId, 
+            hairTypeId
+        });
+
+        return res.send(createdClient);
+        
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+const updateClient = async (req, res, next ) => {
+    const id = req.params.id;
+    const body = req.body;
+    const [numberOfAffectedRows, affectedRows] = await Client.update(body, {
+        where: {id},
+        returning: true, // needed for affectedRows to be populated
+        plain: true // makes sure that the returned instances are just plain objects
+
+    })
+        return res.send(numberOfAffectedRows);
+}
+
+
+const deleteClient = (req, res, next) => {
+    const id = req.params.id;
+    Client.destroy({
+        where: {
+            id,
+        }
+    })
+    .then(() => {
+        res.sendStatus(200);
+    })
+    .catch((error) => next(error));
+
+}
+
+module.exports = {
+    getClients,
+    getClientById,
+    addClient,
+    deleteClient,
+    // updateClient
+}
