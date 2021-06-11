ce13cfd0d3930675e73d25ec2c546e8b4c004f2a
lautaroalcoba <lautaroalcoba@hotmail.com.ar>
2021-06-11 15:50:23 -0300

    componente barberDetail

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 import Home from './components/container/Home/Home';
 import './App.css';
 import NavBar from './components/content/navBar/NavBar';
+import showBarberDetail from './components/content/barberDetail/BarberDetail';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Home}/>
       <Route path="/" component={NavBar}/>
+      <Route path="/Detail" component={showBarberDetail}/>
     </div>   
   );
 }
--- client/src/components/content/barberDetail/BarberDetail.js
+++ client/src/components/content/barberDetail/BarberDetail.js
@@ -9,26 +9,21 @@ function showBarberDetail (props) {
         dispatch(barberDetail(id));
     },[])
     return <div class="md:flex shadow-lg  mx-6 md:mx-auto my-40 max-w-lg md:max-w-2xl h-64">
-    <img class="h-full w-full md:w-1/3  object-cover rounded-lg rounded-r-none pb-5/6" src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg" alt="bag"/>
+    <img class="h-full w-full md:w-1/3  object-cover rounded-lg rounded-r-none pb-5/6" src="https://image.freepik.com/vector-gratis/hombre-barbero-mascota-corte-barberia_165162-68.jpg" alt=""/>
     <div class="w-full md:w-2/3 px-4 py-4 bg-white rounded-lg">
        <div class="flex items-center">
-          <h2 class="text-xl text-gray-800 font-medium mr-auto">Your Travel Buddy</h2>
-          <p class="text-gray-800 font-semibold tracking-tighter">
-             only
-             <i class="text-gray-600 line-through">60$</i>
-             48$
-          </p>
+          <h2 class="text-xl text-gray-800 font-medium mr-auto">Barber Fullname</h2>
        </div>
        <p class="text-sm text-gray-700 mt-4">
           Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequ adipisicing elit. Amet veritatis ipsam reiciendis numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequi.
        </p>
        <div class="flex items-center justify-end mt-4 top-auto">
-          <button class="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">Delete</button>
-          <button class=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">Edit</button>
-          <button class=" bg-blue-600 text-gray-200 px-2 py-2 rounded-md ">Publish</button>
+          <button class="bg-blue-600 text-gray-200 px-4 py-2 rounded mr-auto hover:underline">Styles</button>
+          <button class=" bg-blue-600 text-gray-200 px-2 py-2 rounded-md mr-2">Hair Types</button>
+          <button class=" bg-blue-600 text-gray-200 px-2 py-2 rounded-md ">Face types</button>
        </div>
     </div>
  </div>
 }
 
-export default CreateCategory
+export default showBarberDetail
