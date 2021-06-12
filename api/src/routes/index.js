89fbd74d9441c59e1bbdc84cd75a7ab2b1e342a4
seba4268 <eduardociare871@gmail.com>
2021-06-12 17:11:28 -0300

    acomodando cosas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,24 @@
 const { Router } = require("express");
 
-// Rutas del crud de barberos
-const barbers = require('./barbers');
 
 // Rutas para crear los servicios de un barbero
+
+// Rutas del crud de servicio
 const service = require('./service');
+
+// Rutas del crud de barberos
 const barbers = require("./barbers.js");
+
+// ruta del crud del cliente
 const clients = require('./clients');
+
+// ruta del crud de la cita
 const appointments = require("./appointments");
+
+// ruta del crud del detalle de cita 
 const detailAppointments = require('./detailAppointments');
+
+// ruta del crud de categories
 const categories = require("./categories.js");
 
 
