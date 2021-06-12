c2ebb161f06b5341a2f4229b7286c50ed1abc58f
Diego Lanesan <diegolanesan@gmail.com>
2021-06-12 18:32:37 -0300

    Changes in Home component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Route } from "react-router-dom";
 import Home from './components/container/Home/Home';
 import './App.css';
 import NavBar from './components/content/navBar/NavBar';
+import showBarberDetail from './components/content/barberDetail/BarberDetail';
 import Catalog from './components/content/catalog/catalog';
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={NavBar}/>
+      <Route path="/Detail" component={showBarberDetail}/>
       <Route exact path="/" component={Home}/>
       <Route path="/catalog" component={Catalog} /> {/*hecho para pruebas*/}
     </div>   
