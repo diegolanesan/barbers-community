95cf3a35ccf6c4621fd7e048db5992d18776dbf9
Lmanti <luisangelherreramantilla@gmail.com>
2021-06-13 15:04:25 -0500

    small bug fixed

--- api/src/controllers/barbers.js
+++ api/src/controllers/barbers.js
@@ -6,13 +6,13 @@ const barbers = require("../../data"); // solo pruebas <-------
 // Ruta que devuelve todos los barberos
 
 const getAllBarbers = async(req, res)=>{
-    // const barber = await Barber.findAll();
-    // if(barber){
-    //     res.send(barber)
-    // }else{
-    //     res.status(400).send("No hay barberos en la base de datos")
-    // }
-    res.json(barbers)
+    const barber = await Barber.findAll();
+    if(barber){
+        res.send(barber)
+    }else{
+        res.status(400).send("No hay barberos en la base de datos")
+    }
+    // res.json(barbers)
     // // console.log(json(barbers))
     // console.log(barbers)
 
