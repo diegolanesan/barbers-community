adae0f03d695c09400c71ff03dca177f834915e9
Lmanti <luisangelherreramantilla@gmail.com>
2021-06-13 18:37:25 -0500

    duplication bug fixed

--- client/src/redux/action/barbers.js
+++ client/src/redux/action/barbers.js
@@ -5,8 +5,9 @@ export const POST_BARBER = 'POST_BARBER'
 export const DELETE_BARBER = 'DELETE_BARBER'
 export const PUT_BARBER = 'PUT_BARBER'
 export const FILTER_BARBERS = 'FILTER_BARBERS'
+export const GET_BARBERS_BY_NAME = 'GET_BARBERS_BY_NAME'
+export const GET_BARBERS_BY_TYPE = 'GET_BARBERS_BY_TYPE'
 
-<<<<<<< Updated upstream
 export const postBarber = (body) => (dispatch) => {
     console.log("aaaaaa", body)
         return axios.post("http://localhost:3001/barbers/", body)
@@ -33,8 +34,6 @@ export const putBarber = (id, body) => (dispatch) => {
                 dispatch({ type: "PUT_BARBER", payload: response.data })
             })
 };
-export const GET_BARBERS_BY_NAME = 'GET_BARBERS_BY_NAME'
-export const GET_BARBERS_BY_TYPE = 'GET_BARBERS_BY_TYPE'
 
 export const getBarbers = () => (dispatch) => axios.get('http://localhost:3001/barbers/all')
             .then(res => dispatch({type: GET_BARBERS, payload: res.data}))
@@ -55,8 +54,3 @@ export const getBarbersByType = (type) => (dispath) => {
     axios.get(HOST_BACK + "/barbers/type/" + type)
         .then(res => dispath({type: GET_BARBERS_BY_TYPE, payload: res.data})
         )}
-=======
-export const getBarbers = () => (dispath) => {
-    axios.get(HOST_BACK + "/barbers/all/")
-    .then(resp => dispath({type: GET_BARBERS, payload: resp.data}))}
->>>>>>> Stashed changes
