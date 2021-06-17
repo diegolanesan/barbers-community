1898541c0d6ee75031615dcebf29eb678662e23b
Lmanti <luisangelherreramantilla@gmail.com>
2021-06-17 18:28:03 -0500

    filters added to dashboard

--- client/src/components/content/barberDashboard/barberDashboard.js
+++ client/src/components/content/barberDashboard/barberDashboard.js
@@ -4,18 +4,22 @@ import React, { useState } from 'react'
 const BarberDashboard = () => {
     const buttonStyle = "bg-blue-400 hover:bg-blue-600 text-white py-1 px-0 mx-0 mb-0 w-full"
     const buttonSelected = "bg-blue-800 text-white py-1 px-0 mx-0 mb-0 w-full"
+    const filterButtonStyle = "bg-blue-400 hover:bg-blue-600 text-white py-2 px-5 mx-2 mt-3 mb-3"
+    const filterSelected = "bg-blue-800 text-white py-2 px-5 mx-2 mt-3 mb-3"
+
     const [buttonState, setButtonState] = useState({
-        menu: 'Dashboard'
+        menu: 'Dashboard',
+        filters: 'Haircut'
     })
 
-    const handleMenuClick = (e) => {
+    const handleClick = (e) => {
         if(!e.target.name) e.preventDefault()
         else setButtonState({...buttonState, [e.target.name]: e.target.value})
     }
 
     return (
         <div className="flex h-full " >
-            <div className={`w-1/6 bg-gray-200`} onClick={handleMenuClick} >
+            <div className={`w-1/6 bg-gray-200`} onClick={handleClick} >
                 <div>  
                     <input type='button' value='Dashboard' name='menu' className={buttonState.menu === 'Dashboard' ? buttonSelected : buttonStyle} />
                 </div>
@@ -44,7 +48,31 @@ const BarberDashboard = () => {
                 )}
                 {buttonState.menu === 'Catalog' && (
                     <div>
-                        <div>filtros</div>
+                        <div >
+                            <div className={`w-full justify-center flex`} onClick={handleClick} >
+                                <div>  
+                                    <input type='button' value='Haircut' name='filters' className={buttonState.filters === 'Haircut' ? filterSelected : filterButtonStyle} />
+                                </div>
+                                <div>  
+                                    <input type='button' value='Beard trim' name='filters' className={buttonState.filters === 'Beard trim' ? filterSelected : filterButtonStyle} />
+                                </div>
+                                <div>  
+                                    <input type='button' value='Kids haircuts' name='filters' className={buttonState.filters === 'Kids haircuts' ? filterSelected : filterButtonStyle} />
+                                </div>
+                                <div>  
+                                    <input type='button' value='Coloration' name='filters' className={buttonState.filters === 'Coloration' ? filterSelected : filterButtonStyle} />
+                                </div>
+                                <div>
+                                    <input type='button' value='Tribal trim' name='filters' className={buttonState.filters === 'Tribal trim' ? filterSelected : filterButtonStyle} />
+                                </div>
+                                <div>
+                                    <input type='button' value='Ozone' name='filters' className={buttonState.filters === 'Ozone' ? filterSelected : filterButtonStyle} />
+                                </div>
+                                <div>
+                                    <input type='button' value='Face mask' name='filters' className={buttonState.filters === 'Face mask' ? filterSelected : filterButtonStyle} />
+                                </div>
+                            </div>
+                        </div>
                         <div>servicios</div>
                     </div>
                 )}
