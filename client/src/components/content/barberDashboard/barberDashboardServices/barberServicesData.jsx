7761a21d68c8ea592d0cdc956ab0cb6461564619
Francisco Granja <franbbr110@gmail.com>
2021-06-21 13:00:32 -0300

    Services Price

--- client/src/components/content/barberDashboard/barberDashboardServices/barberServicesData.jsx
+++ client/src/components/content/barberDashboard/barberDashboardServices/barberServicesData.jsx
@@ -42,17 +42,17 @@ const BarberServicesData = ({ filters }) => {
                             />
                             <h4 className="font-bold">{`${n.name}`}</h4>
                             {/* <h4 className="font-bold">{`${n.description}`}</h4> */}
-                            <div className="flex justify-center pt-2">
-                                <svg
+                            {/* <div className="flex justify-center pt-2"> */}
+                            {/* <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     className="h-5 w-5 m-0 text-yellow-400"
                                     viewBox="0 0 20 20"
                                     fill="currentColor"
                                 >
                                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                </svg>
-                                <h6 className="ml-4">{n.price}</h6>
-                            </div>
+                                </svg> */}
+                            {/* <h6 className="ml-4">{n.price}</h6> */}
+                            {/* </div> */}
                             {
                                 appointment.seleccion === false && n.name === appointment.service ?
                                     <button className="bg-red-400 px-2 rounded" onClick={() => setAppointment({ service: "", extraOne: "", extraTwo: "", extraThree: "", seleccion: true })}>Remove</button>
