98de5342d4c19ef057ff059ec42a507dd97289d9
Lmanti <luisangelherreramantilla@gmail.com>
2021-06-20 20:49:41 -0500

    appointments updated

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -4,9 +4,11 @@ import reducerBarberDetail from "./barberDetail";
 import reducerServices from './services'
 import reducerTypes from './types'
 import authReducer from "./auth";
+import appointmentReduer from "./appointment";
 
 function combineReducer(state = {}, action) {
     return {
+      appointments: appointmentReduer(state.appointments, action),
       auth: authReducer(state.auth, action),
       barbers: barbersReducer(state.barbers, action),
       publication: reducerPublication(state.publication,action),
