7731a8a30cd8442961bf33d49fb84827d189916b
lautaro <lautaroalcoba@hotmail.com.ar>
2021-06-21 16:19:44 -0300

    listo

--- client/src/App.js
+++ client/src/App.js
@@ -24,37 +24,34 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-	const location = useLocation();
-	return (
-		<div className="App">
-			{(location.pathname === "/" ||
-				location.pathname === "/catalog" ||
-				location.pathname === "/dashboard" ||
-				location.pathname === "/loginBarbers" ||
-				location.pathname === "/loginClients") && (
-				<Route path="/" component={NavBar} />
-			)}
-			{/* <Route path="/" component={NavBar}/> */}
-			<Route path="/Detail/:id" component={showBarberDetail} />
-			<Route exact path="/" component={Home} />
-			<Route path="/catalog" component={Catalog} /> {/*hecho para pruebas*/}
-			<Route exact path="/admin/barbers" component={BarberTable} />
-			<Route exact path="/register" component={Register} />
-			<Route exact path="/register" component={RegisterClient} />
-			<Route exact path="/BarberConfig/:id" component={BarberConfig} />
-			<Route exact path="/loginBarbers" component={LoginBarbers} />
-			<Route exact path="/loginClients" component={LoginClient} />
-			<Route exact path="/admin/barbers/edit/:id" component={BarberEdit} />
-			<Route exact path="/academic" component={Academic} />
-			<Route exact path="/urban" component={Urban} />
-			<Route exact path="/hair-technician" component={HairTechnician} />
-			<Route exact path="/recovery/:token" component={Recovery} />
-			<Route exact path="/appointment/date" component={AppointmentDate} />
-			<Route exact path="/barbers/dashboard" component={BarberDashboard} />
-			<Route exact path="/clients/dashboard" component={ClientDesk} />
-			<ToastContainer />
-		</div>
-	);
+  const location = useLocation();
+  return (
+    
+    <div className="App">
+      {/* {(location.pathname === "/" || location.pathname === "/catalog") &&  <Route path="/" component={NavBar}/>} */}
+      
+      <Route path="/" component={NavBar}/>
+      <Route path="/Detail/:id" component={showBarberDetail}/>
+      <Route exact path="/" component={Home}/>
+      <Route path="/catalog" component={Catalog} /> {/*hecho para pruebas*/}
+      <Route exact path="/admin/barbers" component={BarberTable} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/registerClient" component={RegisterClient} />
+      <Route exact path="/BarberConfig/:id" component={BarberConfig} />
+      <Route exact path="/loginBarbers" component={LoginBarbers} />
+      <Route exact path="/loginClients" component={LoginClient} />
+      <Route exact path="/admin/barbers/edit/:id" component={BarberEdit} />
+      <Route exact path="/academic" component={Academic} />
+      <Route exact path="/urban" component={Urban} />
+      <Route exact path="/hair-technician" component={HairTechnician} />
+      <Route exact path="/recovery/:token" component={Recovery} />
+      <Route exact path="/appointment/date" component={AppointmentDate} />
+      <Route exact path="/barbers/dashboard" component={BarberDashboard} />
+      <Route exact path="/clients/dashboard" component={ClientDesk} />
+      <ToastContainer />
+    </div> 
+     
+  );
 }
 
 export default App;
