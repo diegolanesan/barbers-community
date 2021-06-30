f9b1074e8eec336fb72a51652dc4a3a1910efe7b
seba4268 <eduardociare871@gmail.com>
2021-06-29 23:10:11 -0300

    arreglar problemas

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -10,17 +10,12 @@ import appointmentReduer from "./appointment";
 import reducerCart from "./cart";
 import adminsReducer from "./admins";
 import reducerCategory from "./categories";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import reviewsReducer from "./reviews"
-=======
 import reducerStyle from "./styles";
 import reducerServiceCRUD from "./serviceCRUD";
->>>>>>> Stashed changes
-=======
 import reducerStyle from "./styles";
 import reducerServiceCRUD from "./serviceCRUD";
->>>>>>> Stashed changes
+
 
 
 function combineReducer(state = {}, action) {
@@ -37,17 +32,12 @@ function combineReducer(state = {}, action) {
       recovery: reducerRecovery(state.recovery, action),
       cart: reducerCart(state.cart, action),
       category: reducerCategory(state.category, action),
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      reviews: reviewsReducer(state.reviews, action)
-=======
+      reviews: reviewsReducer(state.reviews, action),
+      style: reducerStyle(state.style, action),
+      serviceCrud: reducerServiceCRUD(state.serviceCrud, action),
       style: reducerStyle(state.style, action),
       serviceCrud: reducerServiceCRUD(state.serviceCrud, action)
->>>>>>> Stashed changes
-=======
-      style: reducerStyle(state.style, action),
-      serviceCrud: reducerServiceCRUD(state.serviceCrud, action)
->>>>>>> Stashed changes
+
     };
   }
   
