5b235bdec1707040d9f3f2b37b46df5031889553
Lmanti <luisangelherreramantilla@gmail.com>
2021-07-01 14:54:18 -0500

    catalog updated and filters finished

--- client/src/App.js
+++ client/src/App.js
@@ -53,12 +53,12 @@ function App() {
 			<Route path="/" component={NavBar}/>
 			<Route path="/Detail/:id" component={showBarberDetail} />
 			<Route exact path="/" component={Home} />
-			<Route path="/catalog" component={Catalog} /> 
+			<Route path="/catalog" component={newCatalog} /> 
 			<Route exact path="/loginBarbers" component={LoginBarbers} />
 			<Route exact path="/loginClients" component={LoginClient} />
 			<Route exact path="/register" component={Register} />
 			<Route exact path="/clients/register" component={RegisterClient} />
-			<Route exact path="/newCatalog" component={newCatalog} />
+			{/* <Route exact path="/newCatalog" component={newCatalog} /> */}
 			<Route exact path="/guest/cart" component={Cart} />
 			
 			<Route exact path="/clients/dashboard" component={admin.rol === "client" || admin.rol !== "admin" && admin ? ClientDesk : Error} />
